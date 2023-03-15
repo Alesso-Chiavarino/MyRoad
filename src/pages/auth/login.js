b6b6c65fa245543c1f5d7482cb604ec7934ff82e
Alesso-Chiavarino <alessochiavarino@gmail.com>
2023-03-15 13:03:23 -0300

    login part one

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -39,13 +39,27 @@ const login = () => {
 
     return (
         <Layout title={'Login'}>
-            <h1>holap</h1>
-            <form action="" onSubmit={handleSubmit}>
-                <input type="text" name='email' placeholder='email...' onChange={handleChange} />
-                <input type="text" placeholder='password...' name='password' onChange={handleChange} />
-                <button>Login...</button>
-            </form>
-        </Layout>
+            <div className='flex justify-center mt-20'>
+                <div className='bg-black w-fit flex justify-center items-center flex-col min-w-[600px] p-10 rounded-md'>
+                    <h1 className='text-3xl font-extrabold text-white'>User Login</h1>
+                    <form action="" onSubmit={handleSubmit} className='flex flex-col gap-5 p-5 w-full'>
+                        <div className='flex flex-col text-[#D6DEE7] gap-1'>
+                            <label htmlFor="email" className=' font-bold'>Email</label>
+                            <div className='rounded-md bg-[#B8BFC6] p-[1px]'>
+                                <input type="text" name='email' placeholder='email...' onChange={handleChange} className='bg-black p-2 rounded-md w-full outline-none' />
+                            </div>
+                        </div>
+                        <div className='flex flex-col text-[#D6DEE7] gap-1'>
+                            <label htmlFor="password" className='text-[#D6DEE7] font-bold'>Password</label>
+                            <div className='rounded-md bg-[#B8BFC6] p-[1px]'>
+                                <input type="text" placeholder='password...' name='password' onChange={handleChange} className='bg-black p-2 rounded-md w-full outline-none' />
+                            </div>
+                        </div>
+                        <button className='bg-white rounded-md py-2 font-bold'>Login</button>
+                    </form>
+                </div>
+            </div >
+        </Layout >
     )
 }
 
