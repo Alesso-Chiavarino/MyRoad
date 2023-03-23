9d45c8eb03cc8b22e88bfe8f08bd699eef969ff5
Alesso-Chiavarino <alessochiavarino@gmail.com>
2023-03-23 13:32:33 -0300

    bump advances

--- src/components/CareerTable.jsx
+++ src/components/CareerTable.jsx
@@ -1,44 +1,78 @@
 import { useCareer } from "@/context/CareerContext";
 
-const CareerTable = () => {
+const CareerTable = ({ semester }) => {
 
     const { career } = useCareer();
     let partial = 0;
 
-    return (
-        <div className="career-table">
-            <table className="text-white w-full">
-                <thead>
-                    <tr>
-                        <th className="p-3">Semester</th>
-                        <th className="p-3">Subject</th>
-                        <th className="p-3">Calification</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {career[0]?.semesters.map((sem) =>
-                        sem.subjects.map((sub, index) => (
-                            <tr className='text-white border-b border-gray-700 last:border-b-0' key={`${sem.number}-${index}`}>
+    if (!semester) {
+        return (
+            <div className="career-table">
+                <table className="text-white w-full">
+                    <thead>
+                        <tr>
+                            <th className="p-3">Semester</th>
+                            <th className="p-3">Subject</th>
+                            <th className="p-3">Calification</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {career[0]?.semesters.map((sem) =>
+                            sem.subjects.map((sub, index) => (
+                                <tr className='text-white border-b border-gray-700 last:border-b-0' key={index}>
+                                    {index === 0 && (
+                                        <td rowSpan={sem.subjects.length} className="p-3 font-semibold text-lg border-r text-center border-gray-700">
+                                            {sem.number}
+                                        </td>
+                                    )}
+                                    <td className="p-3 border-r border-gray-700">{sub.name}</td>
+                                    <td className="p-3 flex gap-5">
+                                        {sub.califications?.map((cal) => {
+                                            return (
+                                                <span
+                                                    key={cal.value}
+                                                    className="block w-fit px-3 py-2 bg-[#171717] rounded-md"
+                                                >
+                                                    <span className="font-semibold">{cal.name}: {cal.value}</span>
+                                                </span>
+                                            )
+
+                                        })}
+                                    </td>
+                                </tr>
+                            ))
+                        )}
+                    </tbody>
+                </table>
+            </div>
+        )
+    } else {
+        return (
+            <div className="career-table">
+                <table className="text-white w-full">
+                    <thead>
+                        <tr>
+                            <th className="p-3">Semester</th>
+                            <th className="p-3">Subject</th>
+                            <th className="p-3">Calification</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {career[0]?.semesters[semester - 1].subjects.map((sub, index) => (
+                            <tr className='text-white border-b border-gray-700 last:border-b-0' key={index}>
                                 {index === 0 && (
-                                    <td rowSpan={sem.subjects.length} className="p-3 font-semibold text-lg border-r text-center border-gray-700">
-                                        {sem.number}
+                                    <td rowSpan={career[0]?.semesters[semester - 1].subjects.length} className="p-3 font-semibold text-lg border-r text-center border-gray-700">
+                                        {semester}
                                     </td>
                                 )}
                                 <td className="p-3 border-r border-gray-700">{sub.name}</td>
                                 <td className="p-3 flex gap-5">
                                     {sub.califications?.map((cal) => {
-                                        // console.log(cal)
-                                        if (partial === 2) {
-                                            partial = 0
-                                            // return;
-                                        }
-                                        partial++
                                         return (
                                             <span
                                                 key={cal.value}
                                                 className="block w-fit px-3 py-2 bg-[#171717] rounded-md"
                                             >
-                                                {/* {partial}° partial: <span className="font-semibold">{cal.value}</span> */}
                                                 <span className="font-semibold">{cal.name}: {cal.value}</span>
                                             </span>
                                         )
@@ -46,12 +80,12 @@ const CareerTable = () => {
                                     })}
                                 </td>
                             </tr>
-                        ))
-                    )}
-                </tbody>
-            </table>
-        </div>
-    )
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
 };
 
 export default CareerTable;
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -13,21 +13,24 @@ const dashboard = () => {
     const [dataPie, setDataPie] = useState([])
     const [totalApproved, setTotalApproved] = useState(0)
     const [totalSubjects, setTotalSubjects] = useState(0)
+    const [semester, setSemester] = useState(1)
 
     useEffect(() => {
         let totalSubjects = 0;
         let totalApproved = 0;
 
         if (career[0]) {
-            const data = career[0].semesters[0].subjects.map(sub => {
-                return {
-                    id: sub.name,
-                    label: sub.name,
-                    value: sub.duration,
-                    color: 'hsl(215, 70%, 50%)'
-                }
-            })
-            setDataPie(data)
+            if (semester) {
+                const data = career[0].semesters[semester - 1]?.subjects?.map(sub => {
+                    return {
+                        id: sub.name,
+                        label: sub.name,
+                        value: sub.duration,
+                        color: 'hsl(215, 70%, 50%)'
+                    }
+                })
+                setDataPie(data)
+            }
 
             career[0].semesters.map(sem => {
                 sem.subjects.map(sub => {
@@ -41,51 +44,51 @@ const dashboard = () => {
             setTotalSubjects(totalSubjects)
             setTotalApproved(totalApproved)
         }
-    }, [career])
+    }, [career, semester])
 
-
-    // const [dataBump, setDataBump] = useState([])
-
-    // use effect and map career to get data for bump chart
-    // useEffect(() => {
-    //     // function mapDataToBump(data) {
-    //     const bumpDataa = [];
-
-    //     if (career[0]) {
-    //         career[0].semesters.forEach((semester) => {
-    //             semester.subjects.forEach((subject) => {
-    //                 const subjectId = subject.name;
-    //                 const subjectData = {
-    //                     x: `partial ${subject.califications.length}`,
-    //                     y: subject.califications.reduce((acc, calification) => acc + calification.value, 0) / subject.califications.length
-    //                 };
-
-    //                 const existingSubject = bumpDataa.find((bumpSubject) => bumpSubject.id === subjectId);
-    //                 if (existingSubject) {
-    //                     existingSubject.data.push(subjectData);
-    //                 } else {
-    //                     bumpDataa.push({
-    //                         id: subjectId,
-    //                         data: [subjectData]
-    //                     });
-    //                 }
-    //             });
-    //         });
-    //     }
-
-    //     setDataBump(bumpDataa)
-
-    //     // return bumpData;
-    //     // }
-    //     // mapDataToBump()
-
-    // }, [career])
+    const handleSemester = (e) => {
+        setSemester(e.target.value)
+    }
 
 
 
+    const [dataBump, setDataBump] = useState([])
+
+    useEffect(() => {
+        const bumpDataa = [];
+
+        if (career.length > 0) {
+            career[0].semesters[1]?.subjects.map((subject) => {
+                const subjectId = subject.name;
+                const subjectData = [];
+
+                subject?.califications.map((calification) => {
+                    const calificationData = {
+                        x: calification.name,
+                        y: calification.value
+                    };
+                    subjectData?.push(calificationData);
+                });
+
+                const existingSubject = bumpDataa.find((bumpSubject) => bumpSubject.id === subjectId);
+
+                if (existingSubject) {
+                    existingSubject.data.push(...subjectData);
+                } else {
+                    bumpDataa.push({
+                        id: subjectId,
+                        data: subjectData
+                    });
+                }
+            });
+        }
+
+        setDataBump(bumpDataa);
+        console.log(bumpDataa)
+    }, [career, semester]);
 
 
-    const dataBump = [
+    const dataBump2 = [
         {
             "id": "AM-1",
             "data": [
@@ -200,7 +203,19 @@ const dashboard = () => {
     return (
         <Layout title='Dashboard'>
             <div className='container mx-auto pb-10'>
-                <h1 className='text-4xl text-white font-bold my-5'>Overview</h1>
+                <div className='flex justify-between items-center my-5'>
+                    <h1 className='text-4xl text-white font-bold'>Overview</h1>
+                    <div>
+                        <select className="block px-3 py-2 text-[#D6DEE7] font-medium bg-transparent border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-primary-500 focus:border-primary-500 cursor-pointer" onChange={handleSemester} name="Semesters" id="">
+                            {/* <option value="">Select a semester</option> */}
+                            {career[0]?.semesters.map(sem => {
+                                return (
+                                    <option key={sem.number} value={sem.number}>{sem.number}° semester</option>
+                                )
+                            })}
+                        </select>
+                    </div>
+                </div>
                 <div className='grid grid-cols-3 gap-20'>
                     <ProgressPerfilCard totalApproved={totalApproved} totalSubjects={totalSubjects} />
                     <ProgressPerfilCard totalApproved={totalApproved} totalSubjects={totalSubjects} />
@@ -217,7 +232,7 @@ const dashboard = () => {
                     </div>
                 </div>
                 <div className='bg-[#111111] rounded-xl'>
-                    <CareerTable />
+                    <CareerTable semester={semester} />
                 </div>
                 <CalificationForm />
             </div>
