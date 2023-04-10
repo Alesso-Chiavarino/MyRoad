c39ed1b7e459974bb269ec0103e43afb251c3627
alesso-chiavarino <alessochiavarino@gmail.com>
2023-04-10 16:41:29 -0300

    style(index.js): añadir efecto hover al botón de ir al dashboard

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ const Home = () => {
           <div className="w-full flex flex-col gap-5">
             <h1 className='text-white text-6xl font-extrabold'>Control your university success with us</h1>
             <h3 className='text-[#B8BFC6] text-xl font-bold'>Register, search and follow university academic statistics.</h3>
-            <Link href={'/dashboard'} className='text-white font-bold w-fit rounded-md px-4 flex items-center py-3 gap-2 bg-[#7148FC]'>Go to Dashboard <MdArrowForwardIos className="text-xl" /></Link>
+            <Link href={'/dashboard'} className='text-white font-bold w-fit rounded-md px-4 flex items-center py-3 gap-2 bg-[#7148FC] hover:bg-[#7a51ff]'>Go to Dashboard <MdArrowForwardIos className="text-xl" /></Link>
           </div>
 
           <div></div>
