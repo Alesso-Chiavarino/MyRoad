766f597006f1ca963f51fce52993723281f85466
alesso-chiavarino <alessochiavarino@gmail.com>
2023-04-11 21:30:41 -0300

    feat(career.jsx): añadir modal de confirmación para eliminar carrera
    Se añadió un modal de confirmación para eliminar la carrera del usuario. Al hacer clic en el botón "Delete your career", se abre el modal que pregunta si el usuario está seguro de eliminar su carrera. El modal también informa al usuario que esta acción no se puede deshacer y que cambiará la contraseña de la carrera. El usuario debe ingresar su contraseña original para confirmar la eliminación.

--- src/components/settings/Career.jsx
+++ src/components/settings/Career.jsx
@@ -2,10 +2,22 @@ import Link from 'next/link'
 import { useCareer } from '@/context/CareerContext'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
+import ConfirmModal from '../ConfirmModal';
+import { useState } from 'react';
 
 const Career = () => {
 
-  const { deleteCareer } = useCareer()
+  const { deleteCareer, career } = useCareer()
+
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleModal = () => {
+    setIsOpen(!isOpen)
+  }
+
+  const onClose = () => {
+    setIsOpen(false)
+  }
 
   return (
     <div className='text-gray-300 flex flex-col gap-10'>
@@ -22,9 +34,20 @@ const Career = () => {
         <hr className='opacity-60 my-5' />
         <div className='flex flex-col gap-3'>
           <span className='text-sm'>Deleting your career <b>will delete</b> your profile and your feed.</span>
-          <button className='border-[1px] border-gray-300 rounded-md px-4 py-2 text-gray-300 text-sm w-fit font-bold hover:bg-white hover:border-transparent hover:text-black' onClick={deleteCareer}>Delete your career</button>
+          <button className='border-[1px] border-gray-300 rounded-md px-4 py-2 text-gray-300 text-sm w-fit font-bold hover:bg-white hover:border-transparent hover:text-black' onClick={handleModal}>Delete your career</button>
         </div>
       </div>
+      <ConfirmModal onClose={onClose} isOpen={isOpen} title='Are you absolutely sure?' >
+
+        <span className='text-gray-300'>Are you sure you want to delte your career?</span>
+        <span className='text-gray-300'>This action cannot be undone. This <b>will change</b> the {career[0]?.name}<b> password</b>
+        </span>
+        <span className='text-gray-300'>Please type your <b>original</b> password to confirm.</span>
+        <form className='flex flex-col gap-3 items-center' action="">
+          <input name='originalPassword' placeholder='Type your original password' type="password" className='border-[1px] border-gray-300 w-full rounded-md px-2 py-1 text-gray-300 text-sm bg-transparent font-bold outline-none' />
+          <button className='border-[1px] border-gray-300/50 bg-[#111111] w-full rounded-md px-2  mt-2 py-1 text-red-300 text-sm font-bold hover:bg-red-500 hover:border-transparent hover:text-white'>I understand the consequences, change the password</button>
+        </form>
+      </ConfirmModal>
       <ToastContainer />
     </div>
   )
